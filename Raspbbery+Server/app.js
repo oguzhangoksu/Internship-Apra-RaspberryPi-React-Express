67a19aceb0f0e83e62fb1632458b523275b5cf8b
Oğuzhan Göksu <70150316+oguzhangoksu@users.noreply.github.com>
2023-03-06 15:18:06 +0300

    Update app.js

--- Raspbbery+Server/app.js
+++ Raspbbery+Server/app.js
@@ -1,37 +1,44 @@
 var express = require('express');
 var app = express();
 const si = require('systeminformation')
-makineAdi='Enjeksiyon2';
+
+const axios = require('axios');
+const io = require('socket.io-client');
+var Wifi = require('rpi-wifi-connection');
+var wifi = new Wifi();
+const moment = require('moment');
+
+var makineAdi = 'Enjeksiyon1';
+var Jsonbilgi;
 
 
 
 
-
-async function degerDondur(){
+async function bilgiler() {
     //CPU SICAKLIK
-    var cpuSicaklik =await new Promise((resolve)=>{
+    var cpuSicaklik = await new Promise((resolve) => {
         si.cpuTemperature().then(data => {
             resolve(data.main);
         })
     })
 
     //RAM MİKTARI
-    var ramMiktari = await new Promise((resolve)=>{
+    var ramMiktari = await new Promise((resolve) => {
         si.mem().then(data => {
             memTotal = data.total;
             memUsed = data.used;
-           
+
             resolve({
                 memTotal,
                 memUsed
             })
         })
     })
-    
+
     //WIFI KALİTESİ
-    var wifiData= await new Promise((resolve)=>{
+    var wifiData = await new Promise((resolve) => {
         si.wifiNetworks().then(baglantilar => {
-            si.wifiConnections().then(mevcutBaglanti =>{
+            si.wifiConnections().then(mevcutBaglanti => {
                 resolve({
                     baglantilar,
                     mevcutBaglanti
@@ -39,9 +46,9 @@ async function degerDondur(){
             })
         })
     })
-    
+
     //DISK
-    var diskKapasite= await new Promise((resolve)=>{
+    var diskKapasite = await new Promise((resolve) => {
         si.fsSize().then(data => {
             diskSize = data[0].size
             diskSizeAvaliable = data[0].available
@@ -49,41 +56,74 @@ async function degerDondur(){
                 diskSize,
                 diskSizeAvaliable
             })
-          
+
         })
+
     })
-   
-  
+
     return ({
-        makineAdi: makineAdi,
         cpuSicaklik,
         ramMiktari,
         wifiData,
         diskKapasite
     })
 
-  
-
-
 
 }
 
-app.use(express.json());
-
-app.post('/',function (request, response) {
-    if (request.body.password === "abc") { 
-        degerDondur().then((res)=>{
-            console.log(res)
-            response.json(res);
-        })
-    }
-
-    
-    else {
-       response.send("It is NOT your device!")
-    }
-});
+donecek();
 
 
 
+function donecek() {
+    app.use(express.json());
+    app.post('/', function (request, response) {
+        if (request.body.password === "abc") {
+            wifi.getStatus().then((status) => {response.send(status.ip_address)})
+            //socket bağlatısı
+            const socket = io.connect(request.body.ipadress);
+            socket.on('connect', (data, error) => {
+                if (error) throw error
+                console.log('bağlandım')
+
+            })
+            //disconnect olduğunda donecek fonksiyonunu tekrarla
+            socket.on('disconnect', (response) => {
+                socket.disconnect()
+                clearInterval(myInterval)
+                donecek()
+            })
+            var myInterval = setInterval(() => {
+                wifi.getStatus().then((status) => {
+                    bilgiler().then((result) => {
+                        socket.emit('data', {
+                            cihaz_adi: makineAdi,
+                            cihaz_ip: status.ip_address,
+                            status: "online",
+                            bilgiler: result,
+                            kimlik: socket.id,
+                            tarih: `${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+                        })
+                        console.log({
+                            cihaz_adi: makineAdi,
+                            cihaz_ip: status.ip_address,
+                            status: "online",
+                            bilgiler: result,
+                            kimlik: socket.id,
+                            tarih: `${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+                        })
+                    })
+                })
+
+            }, 3000)
+        }
+
+
+        else {
+            response.send("It is NOT your device!")
+        }
+    });
+
+
+}
 app.listen(3005);
